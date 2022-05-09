1955169cfd4ab9cf29fa72e53eeb3ea0929858e1
LargeRaindrop <guyu_shiwai@126.com>
2022-05-09 19:01:15 +0800

    残差结构

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -12,10 +12,22 @@ class DoubleConv(nn.Module):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
-        self.double_conv = nn.Sequential(
+
+        # self.double_conv = nn.Sequential(
+        #     nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+        #     nn.BatchNorm2d(mid_channels),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+        #     nn.BatchNorm2d(out_channels),
+        #     nn.ReLU(inplace=True)
+        # )
+
+        self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
+        )
+        self.conv2 = nn.Sequential(
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
@@ -24,7 +36,8 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         # return self.double_conv(x)
 
-        out = self.double_conv(x)
+        x = self.conv1(x)
+        out = self.conv2(x)
         return x + out
 
 
